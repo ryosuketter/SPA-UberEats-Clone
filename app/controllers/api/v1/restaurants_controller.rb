e040488e76c74bd80ebcac45dada3444ada8b196
ryosuketter <r.shimizu.yale@gmail.com>
2021-02-07 15:30:08 +0900

    restaurant controller

---
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -0,0 +1,13 @@
+module Api
+  module V1
+    class RestaurantsController < ApplicationController
+      def index
+        restaurant = Restaurant.all
+
+        render json: {
+          restaurant: restaurant
+        }, status: :ok
+      end
+    end
+  end
+end
