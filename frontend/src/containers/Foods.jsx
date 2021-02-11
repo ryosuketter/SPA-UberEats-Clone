d25d52ae881f17ca72a2b4b5c7cfaac06cd0b81f
ryosuketter <r.shimizu.yale@gmail.com>
2021-02-11 18:28:34 +0900

    foods api

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
+
+import {
+  initialState as foodsInitialState,
+  foodsActionTyps,
+  foodsReducer,
+} from "../reducers/foods";
+
+// apis
+import { fetchFoods } from "../apis/foods";
+
+import { REQUEST_STATE } from "../constants";
 
 export const Foods = ({ match }) => {
+  const {
+    params: { restaurantsId },
+  } = match;
+
+  const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
+
+  useEffect(() => {
+    dispatch({ type: foodsActionTyps.FETCHING });
+    fetchFoods(restaurantsId).then((data) => {
+      dispatch({
+        type: foodsActionTyps.FETCH_SUCCESS,
+        payload: {
+          foods: data.foods,
+        },
+      });
+    });
+  }, []);
+
   return (
     <>
-      フード一覧
-      <p>restaurantsIdは {match.params.restaurantsId} です</p>
+      {foodsState.fetchState === REQUEST_STATE.LOADING ? (
+        <p>ロード中...</p>
+      ) : (
+        foodsState.foodsList.map((food) => <div key={food.id}>{food.name}</div>)
+      )}
     </>
   );
 };
